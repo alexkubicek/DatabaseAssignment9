0e7703018f7eda4a729dda08ed80f5e7729e1a8e
alexkubicek <127884841+alexkubicek@users.noreply.github.com>
2023-04-23 11:15:50 -0500

    seems to be working, time to submit

--- Kubicek9.py
+++ Kubicek9.py
@@ -1,25 +1,22 @@
+# Alex Kubicek
+# Assignment 9
+
 from abc import ABC
-
 from mrjob.job import MRJob
-from mrjob.step import MRStep
-
-
-# WHERE clause applied as condition in mapper to emit
-#   WHERE yearid > 1900
-# GROUP BY is done by the "shuffle" with the attribute as the key
-#   GROUP BY playerid
-# SELECT clause is handled by the last reducer
-#   SELECT playerid, 3*(SUM(p_H)+SUM(p_BB))/SUM(p_IPOUTS) as whip
-# HAVING clause handled by reducer that does group by
-#   HAVING SUM(p_W) >= 300
 
 
 class MRAssignment9(MRJob, ABC):
 
     def mapper(self, _, line):
         l = line.split(",")
-        if int(l[1]) > 1900:
-            yield l[0], l[1:]
+        playerid = str(l[0])
+        yearid = l[1]
+        w = l[5]
+        h = l[13]
+        bb = l[16]
+        ipouts = l[12]
+        if yearid.isnumeric() and int(yearid) > 1900:
+            yield playerid, [w, h, bb, ipouts]
 
     def reducer(self, key, values):
         sum_h = 0
@@ -27,14 +24,14 @@ class MRAssignment9(MRJob, ABC):
         sum_ipouts = 0
         sum_w = 0
         for v in values:
-            sum_w += v[5]
-            sum_h += v[13]
-            sum_bb += v[16]
-            sum_ipouts += v[12]
-        whip = (3 * (sum_bb + sum_h)) / sum_ipouts
-        if sum_w >= 300:
-            yield key, whip
+            sum_w += int(v[0])
+            sum_h += int(v[1])
+            sum_bb += int(v[2])
+            sum_ipouts += int(v[3])
+        if sum_ipouts > 0:
+            whip = (3 * (sum_bb + sum_h)) / sum_ipouts
+            if sum_w >= 300:
+                yield key, whip
 
 
-if __name__ == '__main__':
-    MRAssignment9.run()
+MRAssignment9.run()
